e58d4232208cd4d7c06b2740b26608673c8e0704
Rodrigo Almeida <rodrigo.almeida@up42.com>
2020-08-21 13:01:21 +0200

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="image-similarity-measures",
+    version="0.0.1",
+    author="UP42",
+    author_email="support@up42.com",
+    description="Evaluation metrics to assess the similarity between two images.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/up42/image-similarity-measures",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    install_requires=["numpy", "rasterio", "scikit-image", "opencv-python", "pyfftw", "phasepack"],
+    python_requires=">=3.6, <3.8",
+    entry_points = {
+        'console_scripts': ['image-similarity-measures=image_similarity_measures.evaluate:main'],
+    }
+)
