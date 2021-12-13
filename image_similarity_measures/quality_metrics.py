cd4a95415fcd2d3e3fd52ea4cf12e4b3d2bdc4f9
nekhtiari <n.ekhtiary@gmail.com>
2021-12-13 21:47:43 +0100

    Add an extra comment for input dimesions

--- image_similarity_measures/quality_metrics.py
+++ image_similarity_measures/quality_metrics.py
@@ -12,6 +12,10 @@ import cv2
 
 
 def _assert_image_shapes_equal(org_img: np.ndarray, pred_img: np.ndarray, metric: str):
+    # shape of the image should be like this (rows, cols, bands)
+    # Please note that: The interpretation of a 3-dimension array read from rasterio is: (bands, rows, columns) while
+    # image processing software like scikit-image, pillow and matplotlib are generally ordered: (rows, columns, bands)
+    # in order efficiently swap the axis order one can use reshape_as_raster, reshape_as_image from rasterio.plot
     msg = (
         f"Cannot calculate {metric}. Input shapes not identical. y_true shape ="
         f"{str(org_img.shape)}, y_pred shape = {str(pred_img.shape)}"
